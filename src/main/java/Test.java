6c52a3a0276383b0d58c5ce14076cb8e18725d83
vhlebnikov <v.khlebnikov@g.nsu.ru>
2023-12-06 14:54:13 +0700

    Initial state

---
+++ src/main/java/Test.java
@@ -0,0 +1,8 @@
+import io.qt.widgets.*;
+public class Test {
+    public static void main(String[] args) {
+        QApplication.initialize(args);
+        QMessageBox.information(null, "QtJambi", "Hello World!");
+        QApplication.shutdown();
+    }
+}
