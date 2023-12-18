ea5c4267948519020faf5dafed87e6fcdd37e69d
Mikhail <evdokimm05@gmail.com>
2023-12-18 16:47:32 +0700

    add vadim-amina files

---
+++ src/main/java/app/backend/entities/ConnectionStorage.java
@@ -0,0 +1,34 @@
+package ru.nsu.dbb.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionStorage {
+    private final List<Connection> connectionList;
+
+    public ConnectionStorage() {
+        this.connectionList = new ArrayList<>();
+    }
+
+    public List<String> getConnectionList() {
+        return connectionList.stream().map(Connection::getName).toList();
+    }
+
+    public Connection getConnection(String connectionName){
+        return connectionList.stream()
+                .filter(element -> element.getName().equals(connectionName))
+                .findFirst().orElse(null);
+    }
+
+    public void removeConnection(String connectionName){
+        connectionList.removeIf(x -> x.getName().equals(connectionName));
+    }
+
+    public void addConnectionToStorage(Connection connection){
+        connectionList.add(connection);
+    }
+
+    public void disconnectAll() {
+        connectionList.forEach(Connection::disconnect);
+    }
+}
